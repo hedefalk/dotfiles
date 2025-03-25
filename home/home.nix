3bc938da84376816ea2fdeeee664867d62bea304
Viktor Hedefalk <hedefalk@gmail.com>
2025-03-25 16:58:06 +0100

    hardcode home folder, does it bugout on the conditional?

--- home/home.nix
+++ home/home.nix
@@ -5,29 +5,34 @@
        # Define dotfiles directory relative to the flake
        dotfilesPath = "${flakePath}/dots"; # If dotfiles is a subdirectory in your flake
   in {
-    home.username = "viktor";
-    home.homeDirectory = pkgs.lib.mkForce (
-        if pkgs.stdenv.isLinux
-        then "/home/viktor"
-        else "/Users/viktor"
-    );
+    home = {
+      username = "viktor";
+      homeDirectory = "/Users/viktor";
+        # home.homeDirectory = pkgs.lib.mkForce (
+        #     if pkgs.stdenv.isLinux
+        #     then "/home/viktor"
+        #     else "/Users/viktor"
+        # );
 
-    home.sessionVariables = {
-        EDITOR = "zed";
-    };
+        file = {
+          ".config/fish".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/fish";
+          ".config/wezterm".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}//wezterm";
+          # ".config/zed/themes".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/fish/.config/zed/themes";
+          ".config/zed/settings.json".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/zed/settings.json";
+          ".config/zed/keymap.json".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/zed/keymap.json";
+          ".config/git/config".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/git/.gitconfig";
+          ".gitignore".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/git/.gitignore";
+      };
 
-    # This value determines the Home Manager release that your configuration is
-    # compatible with. This helps avoid breakage when a new Home Manager release
-    # introduces backwards incompatible changes.
-    #
-    # You should not change this value, even if you update Home Manager. If you do
-    # want to update the value, then make sure to first check the Home Manager
-    # release notes.
-    home.stateVersion = "24.11"; # Please read the comment before changing.
+        sessionVariables = {
+            EDITOR = "zed";
+        };
+
+        stateVersion = "24.11"; # Please read the comment before changing.
 
     # The home.packages option allows you to install Nix packages into your
     # environment.
-    home.packages = with pkgs;
+      packages = with pkgs;
         [
         jq
         devbox
@@ -39,6 +44,7 @@
         then []
         else []
         );
+    };
 
     programs = {
         fish = {
@@ -72,15 +78,6 @@
         };
     };
 
-    home.file = {
-        ".config/fish".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/fish";
-        ".config/wezterm".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}//wezterm";
-        # ".config/zed/themes".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/fish/.config/zed/themes";
-        ".config/zed/settings.json".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/zed/settings.json";
-        ".config/zed/keymap.json".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/zed/keymap.json";
-        ".config/git/config".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/git/.gitconfig";
-        ".gitignore".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/git/.gitignore";
-    };
 
     programs.home-manager.enable = true;
     }
